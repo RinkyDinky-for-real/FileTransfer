57534b5e90825c6adf3fef0d54c9f6d189d292ba
Mathem <mathiashem4@gmail.com>
2025-11-03 22:39:13 +0100

    feat: Export/Sharing har blitt implementert

--- src/components/ImportFilesPrompt.tsx
+++ src/components/ImportFilesPrompt.tsx
@@ -20,15 +20,11 @@ export default function ImportFilesPrompt({
           <Text style={styles.modalTitle}>Import</Text>
           <Text style={styles.modalMessage}>Choose type:</Text>
           <View style={styles.modalButtonContainer}>
-            <Button
-              title="Files"
-              onPress={() => onSubmit("document")}
-              color="#888"
-            />
+            <Button title="Files" onPress={() => onSubmit("document")} />
             <Button title="Media" onPress={() => onSubmit("image")} />
           </View>
           <View style={styles.modalButtonContainer}>
-            <Button title="Cancel" onPress={() => onCancel} color="#888" />
+            <Button title="Cancel" onPress={onCancel} color="#888" />
           </View>
         </View>
       </View>
